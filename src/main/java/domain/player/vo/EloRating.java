c68d487aedbe06cc9910a8d079afe23cf7c646fd
Z019044 <sebmace78@gmail.com>
2026-07-12 15:10:23 +0200

    TDD day 2 : Elo rating should not be negative

--- src/main/java/domain/player/vo/EloRating.java
+++ src/main/java/domain/player/vo/EloRating.java
@@ -1,8 +1,7 @@
 package domain.player.vo;
 
 public record EloRating(int rating) {
-     public EloRating(int rating) {
-
+     public EloRating  {
         if (rating<0) throw new IllegalArgumentException();
     }
 }
