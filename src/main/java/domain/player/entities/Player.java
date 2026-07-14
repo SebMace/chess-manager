8b6480ab368fd11d02081f4faea598313ab59122
Z019044 <sebmace78@gmail.com>
2026-07-14 14:43:31 +0200

    TDD day 3 : Update state of an entity

--- src/main/java/domain/player/entities/Player.java
+++ src/main/java/domain/player/entities/Player.java
@@ -1,5 +1,6 @@
 package domain.player.entities;
 
+import domain.player.vo.EloRating;
 import domain.player.vo.PlayerId;
 
 public class Player {
@@ -7,6 +8,8 @@ public class Player {
     private String firstName;
     private String lastName;
     private PlayerId playerId;
+    private EloRating eloRating;
+    private EloRating eloRatingLastRecorded;
 
     public Player(PlayerId playerId, String firstName, String lastName) {
         this.playerId = playerId;
@@ -14,11 +17,28 @@ public class Player {
         this.lastName = lastName;
     }
 
+    public Player(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
     public String firstName() {
         return firstName;
     }
     public String lastName() {
         return lastName;
+    }
+
+    public void giveEloRating(EloRating eloRating) {
+        this.eloRating = eloRating;
+    }
+
+    public void recordEloRating(EloRating eloRating) {
+        this.eloRatingLastRecorded = this.eloRating;
+        this.eloRating = eloRating;
+    }
+
+    public EloRating eloRating() {
+    return this.eloRating;
     }
 }
--- src/test/java/player/PlayerTests.java
+++ src/test/java/player/PlayerTests.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class PlayerTests {
-    // Day 1 11/07/2026
+    // Day 1 10/07/2026
     @Test
     void should_create_a_player_with_a_first_name_and_a_last_name() {
         Player player = new Player(
@@ -20,9 +20,26 @@ class PlayerTests {
         assertEquals("Magnus", player.firstName());
         assertEquals("Carlsen", player.lastName());
     }
-    // Day 2 12/07/2026
+    // Day 2 11/07/2026
     @Test
     void should_reject_a_negative_elo_rating() {
         assertThrows(IllegalArgumentException.class, () -> new EloRating(-1));
     }
+
+    // Day 3 12/07/2026
+    // Player as an entity is immutable but its state's value object evolves.
+    // Player's identity never changes
+
+    @Test
+    void should_change_the_elo_rating_of_a_player() {
+        //Given : one existing player with an initial elo rating.
+        Player player = new Player("Sébastien", "Macé");
+        player.giveEloRating(new EloRating(1500));
+        // when : his rating is updated
+        player.recordEloRating(new EloRating(1600));
+        // then : tha player gets the new rating
+        assertEquals(new EloRating(1600),player.eloRating());
+    }
+
+    
 }
